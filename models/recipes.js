3111b34a532674ba40b3a950e24ceded148e5518
lyhuyentrangharriet <harriet.embedded.ee@gmail.com>
2022-02-11 08:40:07 +0800

    start work on models

--- models/recipes.js
+++ models/recipes.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-  
+    username: { type: String, unique: true, required: true },
+    author: { type: String, unique: true, required: true },
+    
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
