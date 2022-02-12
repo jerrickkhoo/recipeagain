781574dbf778db0a27df6e3131d810a1ba03755b
lyhuyentrangharriet <harriet.embedded.ee@gmail.com>
2022-02-12 15:18:39 +0800

    completed USER DELETE route in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,21 @@ const router = express.Router();
 const User = require("../models/users.js");
 
 //CRUD for use model
+// to seed User
+const seedUsers = require("../models/seed/seedUsers")
+router.get("/seeduser", async (req,res)=>{
+  seedUsers.forEach((seedUser)=>{
+    seedUser.password = bcrypt.hashSync(seedUser.password,bcrypt.genSaltSync(10))
+  })
+  //console.log(typeof seedUsers[0].favourites)
+  try {
+    const createdSeedUsers = await User.create(seedUsers)
+    res.status(200).json({ status: "ok", message: "seed users created", data: createdSeedUsers});
+  } catch (error) {
+    res.status(400).json({ status: "not ok", message: "fail to create seed user ", error: error});
+  }
+})
+
 //CREATE a new user
 router.post("/", async (req, res) => {
   //TODO: add validate username length is >3, does not exist in the database already
@@ -36,23 +51,40 @@ router.get('/:userID', async(req,res)=>{
     res.status(400).json({ status: "not ok", message: "fail to find user ", error: error});
   }
 })
-//UPDATE a user
+
+//UPDATE a user credentials
 router.put('/:userID', async(req,res)=>{
   const {userID} = req.params
-  const updatedInfo = {
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    favorites: req.body.favorites,
-  }
+  console.log(req.body)
   try{
-    const updatedUser = await User.findOneAndUpdate({id:userID},updatedInfo,{new:true})
+    const updatedUser = await User.findOneAndUpdate({id:userID},{
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      favourites:req.body.favourites, //FIXME: favourites array does not array properly 
+      //$$addToSet: {favorites: [req.body.favorites]},
+    },
+    {new:true})
     res.status(200).json({ status: "ok", message: "user info updated", data: updatedUser})
   } catch(error){
     res.status(400).json({ status: "not ok", message: "fail to update user info", error: error});
   }
 })
+
+//UPDATE need a separate update route to update user favourite? 
+
 //DELETE a user
+router.delete('/:userID', async(req,res)=>{
+  const {userID} = req.params
+  try{
+    const updatedUser = await User.findOneAndDelete({id:userID})
+    res.status(200).json({ status: "ok", message: "user deleted", data: updatedUser})
+  } catch(error){
+    res.status(400).json({ status: "not ok", message: "fail to delete user ", error: error});
+  }
+})
+
+//DELETE a user favourite aka when the user remove a recipe from their favourite
 
 
 //Log in log out 
