589fe779fc77175c7cc403e5bd9d0271f4ee6e25
jerrickkhoo <jerrickkhoo@gmail.com>
2022-02-15 15:41:00 +0800

    need to commit to pull

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Favourites = () => {
-  return <div>Favourites</div>;
-};
+  return (
+    <div>Favourites</div>
+  )
+}
 
-export default Favourites;
+export default Favourites
