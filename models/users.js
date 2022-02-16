9c1c0123e9ae35df8b977d96f2b9c05dd31d0f90
lyhuyentrangharriet <harriet.embedded.ee@gmail.com>
2022-02-16 18:09:46 +0800

    fix bug in new recipe form remove ingre

--- models/users.js
+++ models/users.js
@@ -11,8 +11,6 @@ const userSchema = Schema({
     type: String , 
     required: true,
     unique: true,
-    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    //https://emailregex.com/
   },
   password: {
     type: String, 
